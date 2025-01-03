d4575299f61fcbb60c66cc6aafeec5e1e7610436
DPPereyra <dppereyra@gmail.com>
2025-01-04 03:38:10 +0800

    Add copier template repos

---
+++ services/repositories/repos-copier-templates.tf
@@ -0,0 +1,18 @@
+module "copier-python-application" {
+  source = "./gitrepo"
+
+  name        = "copier-python-application"
+  description = "Template for python application"
+  has_wiki    = true
+  has_issues  = true
+}
+
+
+module "copier-ansible-role" {
+  source = "./gitrepo"
+
+  name        = "copier-python-application"
+  description = "Template for python application"
+  has_wiki    = true
+  has_issues  = true
+}
