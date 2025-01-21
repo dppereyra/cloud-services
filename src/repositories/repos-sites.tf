221d2ed98b71f9e0fc6506bc91e1f5da55240c34
DPPereyra <dppereyra@gmail.com>
2025-01-21 10:36:16 +0800

    Add online resume repo

---
+++ src/repositories/repos-sites.tf
@@ -0,0 +1,8 @@
+module "online-resume" {
+  source = "./gitrepo"
+
+  name        = "online-resume"
+  description = "Online resume using jsonresume"
+  has_wiki    = false
+  has_issues  = false
+}
