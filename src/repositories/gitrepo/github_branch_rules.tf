701a3c329c59f3cbeaf62c03db6962872a538469
DPPereyra <dppereyra@gmail.com>
2025-01-24 15:45:18 +0800

    Switch back to graphql resource

--- src/repositories/gitrepo/github_branch_rules.tf
+++ src/repositories/gitrepo/github_branch_rules.tf
@@ -10,11 +10,12 @@ resource "github_branch_default" "default" {
 }
 
 
-resource "github_branch_protection_v3" "master" {
-  repository = github_repository.github_repo.name
-  branch     = data.github_branch.master.branch
+resource "github_branch_protection" "master" {
+  repository_id = github_repository.github_repo.node_id
+  pattern       = data.github_branch.master.branch
 
   enforce_admins   = true
+  allows_deletions = true
 
   required_status_checks {
     strict = true
