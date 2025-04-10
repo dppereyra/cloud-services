2c9ee47ed9caefa529ece118b762ba263c875c7c
DPPereyra <dppereyra@gmail.com>
2025-04-11 02:40:55 +0800

    Remove duplicate repo

--- src/repositories/repos-sites.tf
+++ src/repositories/repos-sites.tf
@@ -10,8 +10,8 @@ module "online-resume" {
 module "personal-website" {
   source = "./gitrepo"
 
-  name        = "online-resume"
-  description = "Online resume using jsonresume"
+  name        = "personal-website"
+  description = "Personal website"
   has_wiki    = false
   has_issues  = true
 }
