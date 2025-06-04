9a03015f5b261c3f981b71a27e865b2eec2d1d68
DPPereyra <dppereyra@gmail.com>
2025-06-05 00:11:24 +0800

    Disable branch protection for now

--- src/repositories/gitrepo/github_branch_rules.tf
+++ src/repositories/gitrepo/github_branch_rules.tf
@@ -10,24 +10,24 @@ resource "github_branch_default" "default" {
 }
 
 
-resource "github_branch_protection" "master" {
-  repository_id = github_repository.github_repo.node_id
-  pattern       = data.github_branch.master.branch
+// resource "github_branch_protection" "master" {
+//   repository_id = github_repository.github_repo.node_id
+//   pattern       = data.github_branch.master.branch
 
-  enforce_admins   = true
-  allows_deletions = true
+//   enforce_admins   = true
+//   allows_deletions = true
 
-  required_status_checks {
-    strict = true
-  }
+//   required_status_checks {
+//     strict = true
+//   }
 
-  required_pull_request_reviews {
-    dismiss_stale_reviews           = true
-    required_approving_review_count = 0
-  }
+//   required_pull_request_reviews {
+//     dismiss_stale_reviews           = true
+//     required_approving_review_count = 0
+//   }
 
-  depends_on = [
-    github_repository_file.github,
-    github_repository_file.src
-  ]
-}
+//   depends_on = [
+//     github_repository_file.github,
+//     github_repository_file.src
+//   ]
+// }
